da13a6cf0722e56777aa1f028a148dc631c15be1
bkoer <b.koeroglu@outlook.com>
2022-06-29 12:36:10 +0200

    Step3

--- src/GitHubupload.java
+++ src/GitHubupload.java
@@ -1,7 +1,8 @@
 public class GitHubupload {
     public static void main(String[] args) {
-        System.out.println(Step 1);
-        System.out.println(Step2);
+        System.out.println("Step 1");
+        System.out.println("Step2");
+        System.out.println("Step3");
 
 
     }
